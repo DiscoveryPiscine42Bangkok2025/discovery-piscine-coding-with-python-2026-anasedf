69fb07a4d6db1cf629cfbc5ce93b82f2e74858a6
Anasss03 <anasniloh03@gmail.com>
2026-02-12 23:02:42 +0700

    feat: add initial `checkmate.py` and `main.py` files to implement and test the checkmate logic.

---
+++ rush/ex00/main.py
@@ -0,0 +1,14 @@
+from checkmate import checkmate
+
+#board
+def main():
+    board = """\
+    ....
+    .B..
+    ....
+    ...K
+    """
+    checkmate(board)
+
+if __name__ == "__main__":
+    main()
